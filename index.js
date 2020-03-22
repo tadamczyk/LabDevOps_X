a35b412ea48867ec3d73ba345a6e7eaedcb322b9
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2020-03-22 11:42:10 +0100

    Refactored project

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require("express");
 
 const app = express();
 const client = redis.createClient({
-	host: 'my-redis-server',
-	port: 6379
+    host: 'my-redis-server',
+    port: 6379
 });
 
 app.get('/', (req, res) => {
-	res.send("Hello from my web app!");
+    res.send("Hello from my web app!");
 });
 
 app.listen(8080, () => {
-	console.log("Listen on port 8080");
+    console.log("Listen on port 8080");
 });
