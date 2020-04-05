bebcce35937bcbb7a199ffa1e400ca86366a8068
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2020-04-05 11:38:13 +0200

    Initialized frontend app; refactored repository

--- sandbox/index.js
+++ sandbox/index.js
@@ -4,19 +4,17 @@ const process = require("process");
 
 const app = express();
 const client = redis.createClient({
-    host: "my-redis-server",
-    port: 6379
+	host: "my-redis-server",
+	port: 6379
 });
 
 client.set("counter", 0);
 
 app.get("/", (req, res) => {
-    process.exit(0);
-
-    client.get("counter", (err, counterValue) => {
-	    res.send("Counter: " + counterValue);
-	    client.set("counter", parseInt(counterValue) + 1);
-    });
+	client.get("counter", (err, counterValue) => {
+		res.send("Counter: " + counterValue);
+		client.set("counter", parseInt(counterValue) + 1);
+	});
 });
 
 function nwd(a, b) {
@@ -33,9 +31,9 @@ function nwd(a, b) {
 		a = b;
 		b = tmp;
 	}
-	
+
 	return a;
-}
+};
 
 app.get("/nwd/:number1/:number2", (req, res) => {
 	const number1 = req.params.number1;
@@ -53,6 +51,5 @@ app.get("/nwd/:number1/:number2", (req, res) => {
 });
 
 app.listen(8080, () => {
-    console.log("Listen on port 8080");
+	console.log("Listen on port 8080");
 });
-
