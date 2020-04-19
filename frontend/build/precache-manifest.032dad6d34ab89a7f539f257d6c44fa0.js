0eed74bee398a9891682fb505d9c3f249b2d051a
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2020-04-19 09:03:47 +0200

    Added dockerfile and build files

---
+++ frontend/build/precache-manifest.032dad6d34ab89a7f539f257d6c44fa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c41b72a79b906ebaf0f664522baea6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73a05e5a1812b0d8134f",
+    "url": "/static/css/main.84201dcc.chunk.css"
+  },
+  {
+    "revision": "36e75751e40f69941668",
+    "url": "/static/js/2.bf062c3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf062c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a05e5a1812b0d8134f",
+    "url": "/static/js/main.74d911c1.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  },
+  {
+    "revision": "26ef1cbcfa05495818a9d63927b452cb",
+    "url": "/static/media/logo.26ef1cbc.svg"
+  }
+]);
